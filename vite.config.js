ad214d663dea5a9ca0de0360f9349bdca6b35231
Albanta23 <132266850+Albanta23@users.noreply.github.com>
2025-05-15 16:57:31 +0000

    fix: fuerza resolución de jspdf en Vite para Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["jspdf"]
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
   },
-})
+});
